1f3c66e3d6d5962d061dc9296df59b9d8641813b
hzj <huzhenjie@zqzqq.com>
2026-08-05 18:24:51 +0800

    调整AI回复消息链路

--- social-sdk-xianyu-manager/src/main/java/cn/net/rjnetwork/xianyu/manager/product/controller/LocalProductController.java
+++ social-sdk-xianyu-manager/src/main/java/cn/net/rjnetwork/xianyu/manager/product/controller/LocalProductController.java
@@ -9,6 +9,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
